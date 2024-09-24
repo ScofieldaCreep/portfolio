df2d1d2395c767566012575b96de1f4c7b675856
ScofieldaCreep <tju_scofield@outlook.com>
2024-09-25 02:03:20 +0800

    debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,24 @@
-//  Next.js 项目的配置文件，用于自定义 Next.js 的默认行为
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
-  }
-};
-
-// next.config.js
-module.exports = {
+  },
   async rewrites() {
     return {
       beforeFiles: [
         {
           source: '/blog/:path*',
-          destination: 'https://chi-notion-blog.vercel.app/:path*'
+          destination: 'http://website2.com/blog/:path*'
         },
         {
           source: '/blog',
-          destination: 'https://chi-notion-blog.vercel.app/blog'
+          destination: 'http://website2.com/blog'
         },
         {
           source: '/_next/static/:path*',
-          destination: 'https://chi-notion-blog.vercel.app/_next/static/:path*'
+          destination: 'http://website2.com/_next/static/:path*'
         }
       ]
     };
